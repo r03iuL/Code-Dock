5ab2fb2f2956eac9d828120d4359ac13063ef708
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-08-11 02:57:27 +0600

    implemented the FAQ section for homepage

--- src/Homepage/Home/Homepage.jsx
+++ src/Homepage/Home/Homepage.jsx
@@ -1,10 +1,11 @@
 import Blog from "../Blog";
+import Faq from "../Faq";
 
 const Homepage = () => {
     return (
-        <div>
-            <h1>This is homepage.</h1>
+        <div className="mx-auto max-w-7xl ">
             <Blog></Blog>
+            <Faq></Faq>
         </div>
     );
 };
