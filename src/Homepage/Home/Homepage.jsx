fd28d10294895e360415681ce0ad37d5048bea05
istiakantu <istakahmed636@gmail.com>
2023-08-11 09:31:40 +0600

    navbar added with some mistake

--- src/Homepage/Home/Homepage.jsx
+++ src/Homepage/Home/Homepage.jsx
@@ -1,10 +1,9 @@
-import Navbar from "../Navbar";
+
 
 const Homepage = () => {
     return (
         <div>
             <h1>This is homepage.</h1>
-            <Navbar></Navbar>
         </div>
     );
 };
--- src/Homepage/Navbar.jsx
+++ src/Homepage/Navbar.jsx
@@ -1,8 +1,48 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+    const navbar = <div className="flex">
+        <div className="grid lg:flex">
+            <Link to='/'>Features</Link>
+            <Link to='/'>Blog</Link>
+            <Link to='/blog'>About Us</Link>
+        </div>
+        <div className="grid lg:flex">
+            <Link>Code Editor</Link>
+            <Link>Sign Up</Link>
+        </div>
+    </div>
+
     return (
-        <div>
-            <h1>This is Nav</h1>
+        <div className="navbar bg-slate-100 text-black">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-100 rounded-box w-52 gap-5">
+                        {
+                            navbar
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <h2 className='font-bold text-2xl ms-7'>Logo</h2>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-5">
+                    {
+                        navbar
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end mr-2">
+                {
+
+                }
+            </div>
         </div>
     );
 };
