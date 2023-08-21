7fd90d04839380316f7dc3bef465574fa6acc9a9
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-08-21 15:49:43 +0600

    section title component added

--- src/Pages/DashboardPages/DashBoard/DashBoard.jsx
+++ src/Pages/DashboardPages/DashBoard/DashBoard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { RiGitRepositoryLine } from "react-icons/ri";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const DashBoard = () => {
-  return <div className=""></div>;
+  return (
+    <div className="px-[4%] py-[3%]">
+      {/* dashboard details  */}
+      <div className="flex">
+        <div className="box-style">
+          <p className="flex items-center justify-between mb-4">
+            {/* user repositories number */}
+            <span className="font-semibold text-xl">23</span>
+            <span className="bg-violet-200 rounded-md p-2 text-2xl">
+              <RiGitRepositoryLine></RiGitRepositoryLine>
+            </span>
+          </p>
+          <p className="text-gray-500 text-lg">Total Repositories</p>p
+        </div>
+      </div>
+
+      {/* recent in dashboard section  */}
+      <div className="box-style my-[3%]">
+        <SectionTitle heading="Recent Activities"></SectionTitle>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
