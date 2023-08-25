9fb6cce8b15b26bf056f5ca8b3cb518c742e5855
Robiul Islam <robiulislam1806@gmail.com>
2023-08-25 19:46:03 +0600

    Updated hero

--- src/Homepage/Hero.jsx
+++ src/Homepage/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
       <div className="hero min-h-[85vh]">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={img} className="max-w-md rounded-lg " />
-          <div>
+          <div className="flex flex-col px-2">
             <h1 className="text-4xl lg:text-6xl font-bold font-mono">
               Code, Collaborate, Engage & Communicate!{" "}
             </h1>
-            <p className="font-semibold text-lg font-mono py-4">
+            <p className="font-semibold text-lg text-justify font-mono py-4">
               Welcome to Code Dock, where innovation comes alive. Code with
               cutting-edge tools, collaborate seamlessly with your team, engage
               in a vibrant community, and communicate effortlessly. Unleash your
